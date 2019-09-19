277f328a1cfd945b4155b1cfa959dde5b0a8ac3e
Andre Machado <csantos.machado@gmail.com>
2019-09-19 19:17:30 -0300

    Update tests.py

--- {{cookiecutter.project_slug}}/tests.py
+++ {{cookiecutter.project_slug}}/tests.py
@@ -32,7 +32,7 @@ class TestAcceptanceStripe(unittest.TestCase):
     def test_acceptance_checkout_button_was_instantiated(self):
         """Check if checkout button was captured."""
         pattern = re.compile(
-            r"document.getElementById\('checkout-button-sku_}'\);", re.I | re.M)
+            r"document.getElementById\('checkout-button-sku_'\);", re.I | re.M)
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'),
                         msg="You didn't add a checkout button.")
@@ -78,7 +78,7 @@ class AssessmentTestCases(unittest.TestCase):
 
         options = selenium.webdriver.ChromeOptions()
         options.headless = True
-        options.binary_location = os.getenv('CHROME_PATH')
+#         options.binary_location = os.getenv('CHROME_PATH')
         options.add_argument("--window-size=%s" % WINDOW_SIZE)
         options.add_argument("--disable-gpu")
         options.add_argument('--no-sandbox')
@@ -89,7 +89,7 @@ class AssessmentTestCases(unittest.TestCase):
         )
 
     def _get_button_id(self):
-        pattern = re.compile(r"\('checkout-button-sku_\w{14}'\);", re.I | re.M)
+        pattern = re.compile(r"\('checkout-button-sku_'\);", re.I | re.M)
         res = re.search(pattern, self.dom_str)
         return res.group().split("'")[1]
 
